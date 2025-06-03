54926b670979cdbae11e15ba72be167aa1956466
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2025-06-03 13:32:31 +0900

    add ゴールへの遷移まで、完成

--- Assets/_Project/Chapters/ch4_ClimbCat/Scripts/PlayerController.cs
+++ Assets/_Project/Chapters/ch4_ClimbCat/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,9 +9,10 @@ public class PlayerController : MonoBehaviour
     private float _maxWalkSpeed = 2.0f;
 
     public Sprite[] WalkSprites;
+    public Sprite JumpSprite;
     private float _time = 0;
     int idx = 0;
-    SpriteRenderer _spriteRenderer;
+    private SpriteRenderer _spriteRenderer;
 
     void Start()
     {
@@ -22,19 +24,37 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
             _rigid2D.AddForce(transform.up * _jumpForce);
 
         // 左右に働く力が maxWalkSpeed未満なら
         if (_rigid2D.linearVelocityX < _maxWalkSpeed)
             _rigid2D.AddForce(transform.right * _walkForce); // 右に移動
 
-        _time += Time.deltaTime;
-        if(_time > 0.1f)
+        if (_rigid2D.linearVelocityY != 0) // 空中にいる間（上下に働く重力が0じゃない）
         {
-            _time = 0;
-            _spriteRenderer.sprite = WalkSprites[idx];
-            idx = 1 - idx;
+            _spriteRenderer.sprite = JumpSprite;
         }
+        else
+        {
+            _time += Time.deltaTime;
+            if (_time > 0.1f)
+            {
+                _time = 0;
+                _spriteRenderer.sprite = WalkSprites[idx];
+                idx = 1 - idx;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 触れたオブジェクトの名前を出力
+        Debug.Log($"触れました：{collision.gameObject.name}");
+        SceneManager.LoadScene("ClearScene"); // ClearSceneに遷移する
+
+        var pos = transform.position; // ワールドにおける座標取得
+        var localPos = transform.localPosition; // ローカルにおける座標取得
+
     }
 }
